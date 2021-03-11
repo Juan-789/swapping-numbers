74700a7e33a44058b681b66964b993b7a820af49
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-03-11 12:23:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,29 @@
-let num = 0
-let num2 = 0
-let num1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("First #")
+    basic.showNumber(num2)
+})
 input.onButtonPressed(Button.A, function () {
-    num += -1
+    num1 += -1
+    basic.showNumber(num1)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showString("Second #:")
+    basic.showNumber(num1)
+    basic.showString("First #:")
+    basic.showNumber(num2)
 })
 input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index < 2; index++) {
-        num2 = 0
-        num1 = 0
-        num = num1
-        num = num2
-    }
+    num2 = num1
 })
 input.onButtonPressed(Button.B, function () {
-    num += 1
+    num1 += 1
+    basic.showNumber(num1)
 })
-basic.forever(function () {
-    basic.showNumber(num)
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showString("Second #")
+    basic.showNumber(num1)
 })
-basic.forever(function () {
-	
-})
+let num1 = 0
+let num2 = 0
+num2 = 0
+num1 = 0
